e54bd584cb0cacdb2064091dea634522d7befd51
Geordie <ggi28@uclive.ac.nz>
2023-11-16 14:37:13 +1300

    Can now fetch the owner of a league

--- src/schema.ts
+++ src/schema.ts
@@ -12,32 +12,23 @@ const LeagueType = builder.objectRef<League>('LeagueType');
 builder.objectType(UserType, {
   description: 'A user of the legend application',
   fields: (t) => ({
-    email: t.field({
-      type: "String",
-      resolve: (user) => user.email as string,
-    }),
-    name: t.field({
-      type: "String",
-      resolve: (user) => user.name as string,
-    }),
+    email: t.exposeString('email'),
+    name: t.exposeString('name')
   }),
 });
 
 builder.objectType(LeagueType, {
   description: 'A league of users that compete within an organisation',
   fields: (t) => ({
-    name: t.field({
-      type: "String",
-      resolve: (league) => league.name as string,
-    }),
-    city: t.field({
-      type: "String",
-      resolve: (league) => league.city as string,
-    }),
-    owner_id: t.field({
-      type: "Int",
-      resolve: (league) => league.owner_id as number,
-    }),
+    name: t.exposeString('name'),
+    city: t.exposeString('city'),
+    owner: t.field({
+      type: UserType,
+      resolve: async (league) => {
+        const dbResult = await sql`SELECT * FROM "user" WHERE email = ${league.owner_id}`
+        let result = dbResult[0] as User
+        return result
+    }}),
   }),
 });
 
@@ -55,11 +46,10 @@ builder.queryType({
         return returnUserDB;
       },
     }),
-    allLeagues: t.field({
+    leagues: t.field({
       type: [LeagueType],
       resolve: async () => {
         const leagueData = await sql`SELECT * FROM "league"`;
-        
         const leagues = leagueData.map(row => ({
           name: row.name,
           city: row.city,
@@ -67,7 +57,7 @@ builder.queryType({
         }));
         return leagues as League[];
       },
-    })
+    }),
   }),
 });
 
